607fd12f3bf136da27db2b3546f274c09ccda1b4
Felix221123 <felixbaah47@gmail.com>
2025-07-14 23:45:39 +0100

    built the hero section for the app

--- client/src/components/Common/HeroSection.tsx
+++ client/src/components/Common/HeroSection.tsx
@@ -1,14 +1,38 @@
 import React from 'react'
-import { HeroSectionContainer, SiteHeader } from '../../styles/components/HeroSection'
+import { HeroSectionContainer, SiteHeader, TextSection } from '../../styles/components/HeroSection'
+import { CartNotification } from './CartNotif'
+import UserProfile from "../../assets/svg/user-profile.svg"
+import SearchIcon from "../../assets/svg/search.svg"
+import LikedCart from "../../assets/svg/liked.svg"
+import { Header } from '../Text/Header'
+import { Paragraph } from '../Text/Paragraph'
 
-export const HeroSection:React.FC = () => {
+
+export const HeroSection: React.FC = () => {
   return (
     <>
-    <HeroSectionContainer>
-      <SiteHeader>
-        <>home</>
-      </SiteHeader>
-    </HeroSectionContainer>
+    {/*  */}
+      <HeroSectionContainer>
+        <SiteHeader>
+          <header className='font-bold'>PromptCart</header>
+
+          {/* search box */}
+          <div className="searchBox">
+            <img src={SearchIcon} alt="search tab" />
+          </div>
+
+          {/* user profile */}
+          <div className="userProfile">
+            <img className='cursor-pointer' src={UserProfile} alt=" user profile" />
+            <img className='cursor-pointer' src={LikedCart} alt="liked items section" />
+            <CartNotification count={0} />
+          </div>
+        </SiteHeader>
+        <TextSection>
+          <Header text="Your Ideal Shopping is Here" className='hero-header' />
+          <Paragraph text="Discover your ideal product as fast as you can without breaking a sweat." />
+        </TextSection>
+      </HeroSectionContainer>
     </>
   )
 }
--- client/src/styles/components/HeroSection.ts
+++ client/src/styles/components/HeroSection.ts
@@ -1,8 +1,11 @@
 import styled from "styled-components";
 import {
-    backgroundImage,
-    flexCenter,
-    SizeofOriginalContentsForDevicesMoreThan1440px,
+  backgroundImage,
+  flexCenter,
+  fontSize15px,
+  fontSize24px,
+  fontSize60px,
+  SizeofOriginalContentsForDevicesMoreThan1440px,
 } from "../mixin";
 import HeroBgImage from "../../assets/image/hero-bg.jpg"
 
@@ -21,6 +24,37 @@ export const SiteHeader = styled.header`
 
     @media (min-width:768px) {
         padding: 1.75rem 2rem;
+    }
+
+    > header {
+      ${fontSize15px};
+      color: ${props => props.theme.colors.greyishBlack};
+
+      @media (min-width:768px) {
+          ${fontSize24px};
+      }
+    }
+
+    > .userProfile {
+      ${flexCenter("row", "center")};
+      gap: 2rem;
+
+      > img {
+        width: 2.5rem;
+        height: 2.5rem;
+        object-fit: cover;
+        border-radius: 50%;
+      }
+    }
+
+`
+
+export const TextSection = styled.div`
+    ${flexCenter("column", "center")};
+    padding: 13rem 0rem;
+
+    > .hero-header {
+      ${fontSize60px};
     }
 
 `
